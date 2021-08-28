5cca4c0333da29348231b6157d6cac83e1bb3631
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-08-28 09:52:12 +0800

    feat(config): 一些配置信息置于此
    1. 初始化爬取种子
    2. 获取UA
    3. neo4j 连接信息
    4. redis连接信息

---
+++ spider/config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date  : 2021/8/28
+
+START_URL_PREFIX = "https://baike.baidu.com"
+START_URL = "https://baike.baidu.com/item/战狼Ⅱ"
+UA_URL = "http://useragentstring.com/pages/useragentstring.php?name=Chrome"
+
+NEO4J_URL = "bolt://localhost:7687"
+NEO4J_PASSWORD = "neo"
+
+REDIS_HOST = "localhost"
+REDIS_PASSWORD = None
+REDIS_DB = 6
+REDIS_PORT = 6379
